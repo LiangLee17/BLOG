9a1359caa1e9e32312ee3dd1c5bc84a1d6afd263
lianglee17 <lianglee17@qq.com>
2022-03-26 12:44:40 +0800

    about me finished

--- src/main/java/com/liang/blog/service/BlogServiceImpl.java
+++ src/main/java/com/liang/blog/service/BlogServiceImpl.java
@@ -49,7 +49,7 @@ public class BlogServiceImpl implements BlogService {
 
     @Override
     public Blog getBlog(Long id) {
-        return blogRepository.getById(id);
+        return blogRepository.getBlogById(id);
     }
 
     @Override
